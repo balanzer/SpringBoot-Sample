95956c71171ae80a858101a0c4d1374e8bcc1547
Muralitharan Varatharajan <muralitharan.varatharajan@ihg.com>
2017-12-28 07:42:59 -0500

    Added Sample Project

---
+++ it/src/main/java/com/ihg/it/InternalToolsApplication.java
@@ -0,0 +1,12 @@
+package com.ihg.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternalToolsApplication.class, args);
+	}
+}
